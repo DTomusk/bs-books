6872825b01aa73903db36a55f1ffebf1ccafcc2b
Dave <david.tomusk@gmail.com>
2026-02-03 21:36:30 +0000

    Sleep when there are no events

--- api/cmd/worker/main.go
+++ api/cmd/worker/main.go
@@ -14,6 +14,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -67,6 +68,7 @@ func main() {
 
 			if event == nil {
 				slog.Info("No event to process")
+				time.Sleep(5 * time.Second)
 				continue
 			}
 
